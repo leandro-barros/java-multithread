62ab3b1caaf3063c2719b985d1b902d120c81525
Leandro Barros <leandroedbarros@gmail.com>
2022-08-09 21:14:33 -0300

    feat: Create thread with lambida

--- src/main/java/com/example/Threads_1.java
+++ src/main/java/com/example/Threads_1.java
@@ -8,7 +8,15 @@ public class Threads_1 {
         System.out.println(thread.getName());
 
         Thread threadRunnable = new Thread(new MyRunnable());
-        threadRunnable.run();
+        // threadRunnable.run(); Apenas executand mesma thread.
+        threadRunnable.start(); // Executa em uma nova thread.
+
+        // Runnable with lambida
+        Thread thread1 = new Thread(
+                () -> System.out.println("Thread with lambida")
+        );
+        thread1.start();
+        // thread1.start(); No make, throw exception
 
     }
 
