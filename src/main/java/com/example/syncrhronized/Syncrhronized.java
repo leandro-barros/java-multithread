8b89b8a134b5c04a0d39b0226729b5529c02e7fe
Leandro Barros <leandroedbarros@gmail.com>
2022-08-10 19:37:12 -0300

    feat: Create problem concurrency in the thread

---
+++ src/main/java/com/example/syncrhronized/Syncrhronized.java
@@ -0,0 +1,33 @@
+package com.example.syncrhronized;
+
+public class Syncrhronized {
+
+    static int i = -1;
+
+    public static void main(String[] args) {
+        MyRunnable myRunnable = new MyRunnable();
+
+        Thread thread0 = new Thread(myRunnable);
+        Thread thread1 = new Thread(myRunnable);
+        Thread thread2 = new Thread(myRunnable);
+        Thread thread3 = new Thread(myRunnable);
+        Thread thread4 = new Thread(myRunnable);
+
+        thread0.start();
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        thread4.start();
+
+    }
+
+    public static class MyRunnable implements Runnable {
+
+        @Override
+        public void run() {
+            i++;
+            String nameThread = Thread.currentThread().getName();
+            System.out.println(nameThread + ": " + i);
+        }
+    }
+}
