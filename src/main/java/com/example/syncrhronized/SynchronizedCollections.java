8bd07e6e647aed600d986574e0c2b398f999483c
Leandro Barros <leandroedbarros@gmail.com>
2022-08-10 21:59:58 -0300

    feat: Do not use list of type List or Map with multithread

--- src/main/java/com/example/syncrhronized/SynchronizedCollections.java
+++ src/main/java/com/example/syncrhronized/SynchronizedCollections.java
@@ -1,13 +1,18 @@
 package com.example.syncrhronized;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class SynchronizedCollections {
 
+    // Não use List ou Map para multhread
     private static List<String> listThread = new ArrayList<>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        // listThread = Collections.synchronizedList(listThread); forma de a lista sicronizar.
+
         MyRunnable myRunnable = new MyRunnable();
 
         Thread thread0 = new Thread(myRunnable);
@@ -18,13 +23,16 @@ public class SynchronizedCollections {
         thread1.start();
         thread2.start();
 
+        Thread.sleep(500);
+        System.out.println(listThread);
+
     }
 
     public static class MyRunnable implements Runnable {
 
         @Override
         public void run() {
-
+            listThread.add("Add value in list");
         }
     }
 }
