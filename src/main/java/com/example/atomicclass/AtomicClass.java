5d252b53b5fc348b2401fad72d7bd2f8e940b061
Leandro Barros <leandroedbarros@gmail.com>
2022-08-14 20:30:38 -0300

    feat: Preparing code to practice class atomic

--- src/main/java/com/example/atomicclass/AtomicClass.java
+++ src/main/java/com/example/atomicclass/AtomicClass.java
@@ -1,4 +1,29 @@
 package com.example.atomicclass;
 
 public class AtomicClass {
+
+    static int i = -1;
+
+    public static void main(String[] args) {
+        MyRunnable myRunnable = new MyRunnable();
+
+        Thread thread0 = new Thread(myRunnable);
+        Thread thread1 = new Thread(myRunnable);
+        Thread thread2 = new Thread(myRunnable);
+
+        thread0.start();
+        thread1.start();
+        thread2.start();
+
+    }
+
+    public static class MyRunnable implements Runnable {
+        @Override
+        public void run() {
+            i++;
+            String nameThread = Thread.currentThread().getName();
+            System.out.println(nameThread + ": " + i);
+        }
+    }
+
 }
