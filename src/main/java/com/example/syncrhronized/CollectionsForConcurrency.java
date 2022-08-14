9a1f4b353dc25198dea20017181757fe93f88109
Leandro Barros <leandroedbarros@gmail.com>
2022-08-14 20:16:23 -0300

    feat: Used list in the type BlockingQueue with multhread

--- src/main/java/com/example/syncrhronized/CollectionsForConcurrency.java
+++ src/main/java/com/example/syncrhronized/CollectionsForConcurrency.java
@@ -4,13 +4,18 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 public class CollectionsForConcurrency {
 
     // Coleções do tipo Thread-safe
 //    private static List<String> listThread = new CopyOnWriteArrayList<>();
-    private static Map<Integer, String> map = new HashMap<>();
+//    private static Map<Integer, String> map = new ConcurrentHashMap<>();
+        private static BlockingQueue<String> queue = new LinkedBlockingQueue<>();
 
     public static void main(String[] args) throws InterruptedException {
 
@@ -26,7 +31,8 @@ public class CollectionsForConcurrency {
 
         Thread.sleep(500);
 //        System.out.println(listThread);
-        System.out.println(map);
+//        System.out.println(map);
+        System.out.println(queue);
 
     }
 
@@ -35,7 +41,8 @@ public class CollectionsForConcurrency {
         @Override
         public void run() {
 //            listThread.add("Add value in list");
-            map.put(new Random().nextInt(), "Add value in list");
+//            map.put(new Random().nextInt(), "Add value in map");
+            queue.add("Se tiver condições, apoia");
             String name = Thread.currentThread().getName();
             System.out.println(name + " inseriu na lista");
         }
