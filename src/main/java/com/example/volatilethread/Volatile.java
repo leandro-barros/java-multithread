5679189094654fed1bca1a540fc91208bebee0c3
Leandro Barros <leandroedbarros@gmail.com>
2022-08-15 13:51:53 -0300

    feat: Implementing class MyRunnable in Volatile example

--- src/main/java/com/example/volatilethread/Volatile.java
+++ src/main/java/com/example/volatilethread/Volatile.java
@@ -1,4 +1,18 @@
 package com.example.volatilethread;
 
 public class Volatile {
+
+    private static int number = 0;
+    private static boolean prepared = false;
+
+    private static class MyRunnable implements Runnable {
+        @Override
+        public void run() {
+            while (!prepared) {
+                Thread.yield();
+            }
+
+            System.out.println(number);
+        }
+    }
 }
