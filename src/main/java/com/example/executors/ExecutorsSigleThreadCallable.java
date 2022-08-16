582d0aa0dad3075955363edcc81df06f8e694999
Leandro Barros <leandroedbarros@gmail.com>
2022-08-15 21:02:17 -0300

    feat: Implementing thread with Executors

--- src/main/java/com/example/executors/ExecutorsSigleThreadCallable.java
+++ src/main/java/com/example/executors/ExecutorsSigleThreadCallable.java
@@ -1,4 +1,24 @@
 package com.example.executors;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class ExecutorsSigleThreadCallable {
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.execute(new Task());
+
+        executorService.awaitTermination(5, TimeUnit.SECONDS);
+        executorService.shutdown();
+    }
+
+    private static class Task implements Runnable {
+        @Override
+        public void run() {
+            String nameThread = Thread.currentThread().getName();
+            System.out.println(nameThread + ": Executou.");
+        }
+    }
 }
