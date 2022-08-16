852445f6ded884abdf71e1f8c50a98c0a52ee553
Leandro Barros <leandroedbarros@gmail.com>
2022-08-16 14:49:20 -0300

    feat: Implementing Executors.newFixedThreadPool

--- src/main/java/com/example/executors/multhread/ExecutorsMultithread.java
+++ src/main/java/com/example/executors/multhread/ExecutorsMultithread.java
@@ -1,8 +1,23 @@
 package com.example.executors.multhread;
 
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class ExecutorsMultithread {
 
     public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(4);
 
+    }
+
+    public static class Task implements Callable<String> {
+        @Override
+        public String call() throws Exception {
+            String nameThread = Thread.currentThread().getName();
+            int number = new Random().nextInt(1000);
+            return "(Nome Thread: " + nameThread + ", número Random: " + number + ")";
+        }
     }
 }
