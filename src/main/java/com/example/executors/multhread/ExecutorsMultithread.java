4c24e9556f2e0ffcdfb5948ba263fba93fdf2c10
Leandro Barros <leandroedbarros@gmail.com>
2022-08-16 14:57:24 -0300

    feat: Create task with Executor multithread

--- src/main/java/com/example/executors/multhread/ExecutorsMultithread.java
+++ src/main/java/com/example/executors/multhread/ExecutorsMultithread.java
@@ -1,15 +1,19 @@
 package com.example.executors.multhread;
 
 import java.util.Random;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 public class ExecutorsMultithread {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         ExecutorService executor = Executors.newFixedThreadPool(4);
-
+        Future<String> future1 = executor.submit(new Task());
+        Future<String> future2 = executor.submit(new Task());
+        Future<String> future3 = executor.submit(new Task());
+        System.out.println(future1.get());
+        System.out.println(future2.get());
+        System.out.println(future3.get());
+        executor.shutdown();
     }
 
     public static class Task implements Callable<String> {
