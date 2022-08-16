2b4c1084c1bebcfc3c678cf25e6d74c9d5961619
Leandro Barros <leandroedbarros@gmail.com>
2022-08-16 18:53:00 -0300

    feat: Organization examples

--- src/main/java/com/example/executors/multhread/ExecutorsMultithread.java
+++ src/main/java/com/example/executors/multhread/ExecutorsMultithread.java
@@ -10,6 +10,7 @@ public class ExecutorsMultithread {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         ExecutorService executor = null;
         try {
+//            -------------------------------------- Primeiro exemplo
 //            executor = Executors.newFixedThreadPool(4);
 //            executor = Executors.newCachedThreadPool(); // Cria thread até quando precisar no que pode ser um problema
 //            Future<String> future1 = executor.submit(new Task());
@@ -19,6 +20,23 @@ public class ExecutorsMultithread {
 //            System.out.println(future2.get());
 //            System.out.println(future3.get());
 //            executor.shutdown();
+
+//            -------------------------------------- Segundo exemplo
+//            executor = Executors.newCachedThreadPool();
+//
+//            List<Task> tasks = new ArrayList<>();
+//            for (int i = 0; i < 100; i++) {
+//                tasks.add(new Task());
+//            }
+//
+//            List<Future<String>> futures = executor.invokeAll(tasks);
+//
+//            for (Future<String> future : futures) {
+//                System.out.println(future.get());
+//            }
+//            executor.shutdown();
+
+//            -------------------------------------- Terceiro exemplo
 
             executor = Executors.newCachedThreadPool();
 
