944305a9744dc1e6591372b07fca2e6349f859dc
Leandro Barros <leandroedbarros@gmail.com>
2022-08-17 20:43:37 -0300

    refactor: Saved example with callable

--- src/main/java/com/example/executors/schedule/ExecutorsSchedule.java
+++ src/main/java/com/example/executors/schedule/ExecutorsSchedule.java
@@ -23,4 +23,24 @@ public class ExecutorsSchedule {
             return "(Nome Thread: " + nameThread + ", número Random: " + number + ")";
         }
     }
+
+// **************** Schedule com  Callable ******************
+//    public static void main(String[] args) throws ExecutionException, InterruptedException {
+//        ScheduledExecutorService executor = Executors.newScheduledThreadPool(3);
+//
+//        ScheduledFuture<String> future = executor.schedule(new Task(), 2, TimeUnit.SECONDS);
+//
+//        System.out.println(future.get());
+//
+//        executor.shutdown();
+//    }
+//
+//    public static class Task implements Callable<String> {
+//        @Override
+//        public String call() throws Exception {
+//            String nameThread = Thread.currentThread().getName();
+//            int number = new Random().nextInt(1000);
+//            return "(Nome Thread: " + nameThread + ", número Random: " + number + ")";
+//        }
+//    }
 }
