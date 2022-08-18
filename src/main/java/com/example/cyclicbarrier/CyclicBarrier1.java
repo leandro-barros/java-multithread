8e8ac7639fe79b6f1fe59ff2eb258a1e0f47d82c
Leandro Barros <leandroedbarros@gmail.com>
2022-08-17 22:30:48 -0300

    feat: Example for CyclicBarrier create

--- src/main/java/com/example/cyclicbarrier/CyclicBarrier1.java
+++ src/main/java/com/example/cyclicbarrier/CyclicBarrier1.java
@@ -1,9 +1,28 @@
 package com.example.cyclicbarrier;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class CyclicBarrier1 {
 
     public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(3);
 
+        Runnable r1 = () -> {
+            System.out.println(432d * 3d);
+        };
+        Runnable r2 = () -> {
+            System.out.println(Math.pow(3d, 14d));
+        };
+        Runnable r3 = () -> {
+            System.out.println(45d * 127d / 12d);
+        };
+
+        executor.submit(r1);
+        executor.submit(r2);
+        executor.submit(r3);
+
+        executor.shutdown();
     }
 
 }
