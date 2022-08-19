da8ea299c568c085e345ecbdf5dc65c9aa3f77eb
Leandro Barros <leandroedbarros@gmail.com>
2022-08-18 21:26:28 -0300

    feat: Create class CyclicBarrier3 to example

---
+++ src/main/java/com/example/cyclicbarrier/CyclicBarrier3.java
@@ -0,0 +1,58 @@
+package com.example.cyclicbarrier;
+
+import java.util.concurrent.*;
+
+public class CyclicBarrier3 {
+
+    private static BlockingQueue<Double> result = new LinkedBlockingQueue<>();
+
+    public static void main(String[] args) {
+
+        Runnable finnaly = () -> {
+            System.out.println("Somando tudo");
+
+            double resultFinnaly = 0;
+            resultFinnaly += result.poll();
+            resultFinnaly += result.poll();
+            resultFinnaly += result.poll();
+
+            System.out.println("Processamento finalizado. Resultado final: " + resultFinnaly);
+        };
+
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(3, finnaly);
+
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+
+        Runnable r1 = () -> {
+            result.add(432d * 3d);
+            await(cyclicBarrier);
+            System.out.println("Terminei o processamento.");
+        };
+        Runnable r2 = () -> {
+            result.add(Math.pow(3d, 14d));
+            await(cyclicBarrier);
+            System.out.println("Terminei o processamento.");
+        };
+        Runnable r3 = () -> {
+            result.add(45d * 127d / 12d);
+            await(cyclicBarrier);
+            System.out.println("Terminei o processamento.");
+        };
+
+        executor.submit(r1);
+        executor.submit(r2);
+        executor.submit(r3);
+
+        executor.shutdown();
+    }
+
+    private static void await(CyclicBarrier cyclicBarrier) {
+        try {
+            cyclicBarrier.await();
+        } catch (InterruptedException | BrokenBarrierException e) {
+            Thread.currentThread().interrupt();
+            e.printStackTrace();
+        }
+    }
+
+}
