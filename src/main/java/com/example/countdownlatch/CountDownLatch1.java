850e5cd38a742069e1ea46d346f5f95cd6ff8fc4
Leandro Barros <leandroedbarros@gmail.com>
2022-08-18 22:12:15 -0300

    feat: Implementing schedule to CountDownLatch

--- src/main/java/com/example/countdownlatch/CountDownLatch1.java
+++ src/main/java/com/example/countdownlatch/CountDownLatch1.java
@@ -1,4 +1,24 @@
 package com.example.countdownlatch;
 
+import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class CountDownLatch1 {
+
+    private static int i = 0;
+
+    public static void main(String[] args) {
+
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(3);
+
+        Runnable runnable = () -> {
+            int j = new Random().nextInt(1000);
+            int x = i * j;
+            System.out.println(i + " x " + j + " = " + x);
+        };
+
+        executor.scheduleAtFixedRate(runnable, 0, 1, TimeUnit.SECONDS);
+    }
 }
