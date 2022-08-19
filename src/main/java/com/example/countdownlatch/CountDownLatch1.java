b1b0b51a097ccf2e072e06eac32b334d0c37abdc
Leandro Barros <leandroedbarros@gmail.com>
2022-08-18 22:33:10 -0300

    feat: Using CountDownLatch

--- src/main/java/com/example/countdownlatch/CountDownLatch1.java
+++ src/main/java/com/example/countdownlatch/CountDownLatch1.java
@@ -1,6 +1,7 @@
 package com.example.countdownlatch;
 
 import java.util.Random;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -8,6 +9,8 @@ import java.util.concurrent.TimeUnit;
 public class CountDownLatch1 {
 
     private static volatile int i = 0;
+
+    private static CountDownLatch countDownLatch = new CountDownLatch(3);
 
     public static void main(String[] args) {
 
@@ -17,14 +20,28 @@ public class CountDownLatch1 {
             int j = new Random().nextInt(1000);
             int x = i * j;
             System.out.println(i + " x " + j + " = " + x);
+            countDownLatch.countDown();
         };
 
         executor.scheduleAtFixedRate(runnable, 0, 1, TimeUnit.SECONDS);
+
+        while (true) {
+            await();
+            i = new Random().nextInt(100);
+        }
 
 //        while (true) {
 //            sleep();
 //            i = new Random().nextInt(100);
 //        }
+    }
+
+    private static void await() {
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 //    private static void sleep() {
