b03bdf188277888bb41aaa4f3e55ead34790be69
Leandro Barros <leandroedbarros@gmail.com>
2022-08-22 19:44:05 -0300

    feat: Example two semaphore

---
+++ src/main/java/com/example/semaphore/Semaphore2.java
@@ -0,0 +1,52 @@
+package com.example.semaphore;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+public class Semaphore2 {
+
+    private static final Semaphore SEMAPHORE = new Semaphore(3);
+
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+        Runnable r1 = () -> {
+            String name = Thread.currentThread().getName();
+            int idUser = new Random().nextInt(10000);
+
+            acquire();
+            System.out.println("Print nome usuário (" + idUser + ") com thread " + name);
+            sleep();
+            SEMAPHORE.release();
+        };
+
+
+        for (int i = 0; i < 500; i++) {
+            executor.execute(r1);
+        }
+
+        executor.shutdown();
+    }
+
+    private static void acquire() {
+        try {
+            SEMAPHORE.acquire();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            e.printStackTrace();
+        }
+    }
+
+    private static void sleep() {
+        try {
+            int time = new Random().nextInt(6);
+            time++;
+            Thread.sleep(1000 * time);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            e.printStackTrace();
+        }
+    }
+}
