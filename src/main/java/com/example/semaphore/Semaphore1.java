3a862b625a31df156d99db6a99f60a6be8805896
Leandro Barros <leandroedbarros@gmail.com>
2022-08-22 19:36:20 -0300

    feat: Using method acquire()

--- src/main/java/com/example/semaphore/Semaphore1.java
+++ src/main/java/com/example/semaphore/Semaphore1.java
@@ -3,8 +3,11 @@ package com.example.semaphore;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
 
 public class Semaphore1 {
+
+    private static final Semaphore SEMAPHORE = new Semaphore(3);
 
     public static void main(String[] args) {
         ExecutorService executor = Executors.newCachedThreadPool();
@@ -12,13 +15,25 @@ public class Semaphore1 {
         Runnable r1 = () -> {
             String name = Thread.currentThread().getName();
             int idUser = new Random().nextInt(10000);
+
+            acquire();
             System.out.println("Print nome usuário (" + idUser + ") com thread " + name);
         };
+
 
         for (int i = 0; i < 500; i++) {
             executor.execute(r1);
         }
 
         executor.shutdown();
+    }
+
+    private static void acquire() {
+        try {
+            SEMAPHORE.acquire();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            e.printStackTrace();
+        }
     }
 }
