93b9cc1bd35c8cb0fb222b71049adadcf6e02400
Leandro Barros <leandroedbarros@gmail.com>
2022-08-22 19:24:42 -0300

    feat: Starting example semaphore

--- src/main/java/com/example/semaphore/Semaphore1.java
+++ src/main/java/com/example/semaphore/Semaphore1.java
@@ -1,8 +1,24 @@
 package com.example.semaphore;
 
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Semaphore1 {
 
     public static void main(String[] args) {
+        ExecutorService executor = Executors.newCachedThreadPool();
 
+        Runnable r1 = () -> {
+            String name = Thread.currentThread().getName();
+            int idUser = new Random().nextInt(10000);
+            System.out.println("Print nome usuário (" + idUser + ") com thread " + name);
+        };
+
+        for (int i = 0; i < 500; i++) {
+            executor.execute(r1);
+        }
+
+        executor.shutdown();
     }
 }
