2e09d65548deed5b87dd27e2513ac28e862c3ae3
Leandro Barros <leandroedbarros@gmail.com>
2022-08-23 15:23:49 -0300

    feat: Implementing example Lock

--- src/main/java/com/example/lock/ReentrantLock1.java
+++ src/main/java/com/example/lock/ReentrantLock1.java
@@ -1,8 +1,26 @@
 package com.example.lock;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class ReentrantLock1 {
 
+    private static int i = -1;
+
     public static void main(String[] args) {
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+        Runnable runnable = () -> {
+            String nameThread = Thread.currentThread().getName();
+            i++;
+            System.out.println(nameThread + " lendo incremento " + i);
+        };
+
+        for (int j = 0; j < 6; j++) {
+            executor.execute(runnable);
+        }
+
+        executor.shutdown();
 
     }
 
