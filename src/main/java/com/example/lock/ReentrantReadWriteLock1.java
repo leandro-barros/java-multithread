4510ba98a73fc94b8e0aa5a10992a114ab579a5d
Leandro Barros <leandroedbarros@gmail.com>
2022-08-23 15:49:25 -0300

    feat: Example ReadWriteLock

--- src/main/java/com/example/lock/ReentrantReadWriteLock1.java
+++ src/main/java/com/example/lock/ReentrantReadWriteLock1.java
@@ -3,23 +3,25 @@ package com.example.lock;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
-public class ReentrantReadWriteLock {
+public class ReentrantReadWriteLock1 {
 
     private static int i = -1;
 
-    private static Lock lock = new ReentrantLock();
+    private static ReadWriteLock lock = new ReentrantReadWriteLock();
 
     public static void main(String[] args) {
         ExecutorService executor = Executors.newCachedThreadPool();
 
         Runnable runnable = () -> {
-            lock.lock();
+            Lock writeLock = lock.writeLock();
+            writeLock.lock();
             String nameThread = Thread.currentThread().getName();
             i++;
             System.out.println(nameThread + " lendo incremento " + i);
-            lock.unlock();
+            writeLock.unlock();
         };
 
         for (int j = 0; j < 6; j++) {
