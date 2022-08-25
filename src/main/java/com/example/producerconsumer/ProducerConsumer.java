abd7b422d2ed90f0def16171a8ba113b737d0f59
Leandro Barros <leandroedbarros@gmail.com>
2022-08-25 15:26:56 -0300

    feat: Implementing example producer and consumer

--- src/main/java/com/example/producerconsumer/ProducerConsumer.java
+++ src/main/java/com/example/producerconsumer/ProducerConsumer.java
@@ -1,20 +1,55 @@
 package com.example.producerconsumer;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.util.Random;
 
 public class ProducerConsumer {
+
+    private static final List<Integer> INTEGER_LIST = new ArrayList<>(5);
+    private static boolean producing = true;
+    private static boolean consuming = true;
 
     public static void main(String[] args) {
 
         Thread producer = new Thread(() -> {
             while (true) {
+                simulateProcessing();
 
+                if (producing) {
+                    int number = new Random().nextInt(10000);
+                    INTEGER_LIST.add(number);
+
+                    if (INTEGER_LIST.size() == 5) {
+                        producing = false;
+                    }
+
+                    if (INTEGER_LIST.size() == 1) {
+                        consuming = true;
+                    }
+                }
             }
         });
 
         Thread consumer = new Thread(() -> {
             while (true) {
+                simulateProcessing();
 
+                if (consuming) {
+                    Optional<Integer> number = INTEGER_LIST.stream().findFirst();
+                    number.ifPresent(n -> {
+                        INTEGER_LIST.remove(n);
+                    });
+
+                    if (INTEGER_LIST.size() == 0) {
+                        consuming = false;
+                    }
+
+                    if (INTEGER_LIST.size() == 4) {
+                        producing = true;
+                    }
+                }
             }
         });
 
