a0891c1e829c6e851dcae7c3d3c2f3e9294604fd
Leandro Barros <leandroedbarros@gmail.com>
2022-08-25 15:05:22 -0300

    feat: Creating two threads to example queue

--- src/main/java/com/example/producerconsumer/ProducerConsumer.java
+++ src/main/java/com/example/producerconsumer/ProducerConsumer.java
@@ -4,6 +4,20 @@ public class ProducerConsumer {
 
     public static void main(String[] args) {
 
+        Thread producer = new Thread(() -> {
+            while (true) {
+
+            }
+        });
+
+        Thread consumer = new Thread(() -> {
+            while (true) {
+
+            }
+        });
+
+        producer.start();
+        consumer.start();
     }
 
 }
