ae6b0d6e855b3a20a64b397db8f11c53b014f817
Leandro Barros <leandroedbarros@gmail.com>
2022-08-25 15:52:00 -0300

    refactor: Inserting try catch in example Deadlock

--- src/main/java/com/example/producerconsumer/ProducerConsumer.java
+++ src/main/java/com/example/producerconsumer/ProducerConsumer.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Random;
 
+// Problema de Race Condition(condição de corrida/concorrência) e DeadLock
 public class ProducerConsumer {
 
     private static final List<Integer> INTEGER_LIST = new ArrayList<>(5);
@@ -14,52 +15,60 @@ public class ProducerConsumer {
     public static void main(String[] args) {
 
         Thread producer = new Thread(() -> {
-            while (true) {
-                simulateProcessing();
+            try {
+                while (true) {
+                    simulateProcessing();
 
-                if (producing) {
-                    System.out.println("Produzindo");
-                    int number = new Random().nextInt(10000);
-                    INTEGER_LIST.add(number);
+                    if (producing) {
+                        System.out.println("Produzindo");
+                        int number = new Random().nextInt(10000);
+                        INTEGER_LIST.add(number);
 
-                    if (INTEGER_LIST.size() == 5) {
-                        System.out.println("Pausando produtor");
-                        producing = false;
+                        if (INTEGER_LIST.size() == 5) {
+                            System.out.println("Pausando produtor");
+                            producing = false;
+                        }
+
+                        if (INTEGER_LIST.size() == 1) {
+                            System.out.println("Iniciando consumidor");
+                            consuming = true;
+                        }
+                    } else {
+                        System.out.println("-------- Produtor dormindo!");
                     }
-
-                    if (INTEGER_LIST.size() == 1) {
-                        System.out.println("Iniciando consumidor");
-                        consuming = true;
-                    }
-                } else {
-                    System.out.println("-------- Produtor dormindo!");
                 }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
             }
         });
 
         Thread consumer = new Thread(() -> {
-            while (true) {
-                simulateProcessing();
+            try {
+                while (true) {
+                    simulateProcessing();
 
-                if (consuming) {
-                    System.out.println("Consumindo");
-                    Optional<Integer> number = INTEGER_LIST.stream().findFirst();
-                    number.ifPresent(n -> {
-                        INTEGER_LIST.remove(n);
-                    });
+                    if (consuming) {
+                        System.out.println("Consumindo");
+                        Optional<Integer> number = INTEGER_LIST.stream().findFirst();
+                        number.ifPresent(n -> {
+                            INTEGER_LIST.remove(n);
+                        });
 
-                    if (INTEGER_LIST.size() == 0) {
-                        System.out.println("Pausando consumidor");
-                        consuming = false;
+                        if (INTEGER_LIST.size() == 0) {
+                            System.out.println("Pausando consumidor");
+                            consuming = false;
+                        }
+
+                        if (INTEGER_LIST.size() == 4) {
+                            System.out.println("Iniciando produtor");
+                            producing = true;
+                        }
+                    } else {
+                        System.out.println("******** Consumidor dormindo!");
                     }
-
-                    if (INTEGER_LIST.size() == 4) {
-                        System.out.println("Iniciando produtor");
-                        producing = true;
-                    }
-                } else {
-                    System.out.println("******** Consumidor dormindo!");
                 }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
             }
         });
 
