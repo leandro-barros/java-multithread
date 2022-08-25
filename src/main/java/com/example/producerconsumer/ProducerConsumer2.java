e1f4673fc6612189e017582816851b17248e2afa
Leandro Barros <leandroedbarros@gmail.com>
2022-08-25 15:59:02 -0300

    feat: Creating class to example in the Critical Section and Mutual Exclusion

---
+++ src/main/java/com/example/producerconsumer/ProducerConsumer2.java
@@ -0,0 +1,11 @@
+package com.example.producerconsumer;
+
+/**
+ * 2) Região critica e exclusão mútua.
+ */
+public class ProducerConsumer2 {
+
+    public static void main(String[] args) {
+
+    }
+}
