b3a7970c70a85b3f635e3844de5bf4af54a18a0f
Leandro Barros <leandroedbarros@gmail.com>
2022-08-26 14:19:42 -0300

    feat: Starting example in the Critical Section and Mutual Exclusion

--- src/main/java/com/example/producerconsumer/ProducerConsumer2.java
+++ src/main/java/com/example/producerconsumer/ProducerConsumer2.java
@@ -1,5 +1,10 @@
 package com.example.producerconsumer;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
+
 /**
  * 2) Região critica e exclusão mútua.
  */
@@ -7,5 +12,82 @@ public class ProducerConsumer2 {
 
     public static void main(String[] args) {
 
+        private static final List<Integer> INTEGER_LIST = new ArrayList<>(5);
+        private static boolean producing = true;
+        private static boolean consuming = true;
+
+        public static void main(String[] args) {
+
+            Thread producer = new Thread(() -> {
+                try {
+                    while (true) {
+                        simulateProcessing();
+
+                        if (producing) {
+                            System.out.println("Produzindo");
+                            int number = new Random().nextInt(10000);
+                            INTEGER_LIST.add(number);
+
+                            if (INTEGER_LIST.size() == 5) {
+                                System.out.println("Pausando produtor");
+                                producing = false;
+                            }
+
+                            if (INTEGER_LIST.size() == 1) {
+                                System.out.println("Iniciando consumidor");
+                                consuming = true;
+                            }
+                        } else {
+                            System.out.println("-------- Produtor dormindo!");
+                        }
+                    }
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            });
+
+            Thread consumer = new Thread(() -> {
+                try {
+                    while (true) {
+                        simulateProcessing();
+
+                        if (consuming) {
+                            System.out.println("Consumindo");
+                            Optional<Integer> number = INTEGER_LIST.stream().findFirst();
+                            number.ifPresent(n -> {
+                                INTEGER_LIST.remove(n);
+                            });
+
+                            if (INTEGER_LIST.size() == 0) {
+                                System.out.println("Pausando consumidor");
+                                consuming = false;
+                            }
+
+                            if (INTEGER_LIST.size() == 4) {
+                                System.out.println("Iniciando produtor");
+                                producing = true;
+                            }
+                        } else {
+                            System.out.println("******** Consumidor dormindo!");
+                        }
+                    }
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            });
+
+            producer.start();
+            consumer.start();
+        }
+
+        private static final void simulateProcessing() {
+            int time = new Random().nextInt(10);
+            try {
+                Thread.sleep(time);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                e.printStackTrace();
+            }
+        }
     }
 }
