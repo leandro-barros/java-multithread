df0bd6977b844dbf77b753ea1d778390aa9186bd
Leandro Barros <leandroedbarros@gmail.com>
2022-08-26 15:27:04 -0300

    feat: Creating class to example in the refactoring

---
+++ src/main/java/com/example/producerconsumer/ProducerConsumer3.java
@@ -0,0 +1,11 @@
+package com.example.producerconsumer;
+
+/**
+ * 3) Usando as ferramentas certas em comparação com o exemplo 1 e 2
+ */
+public class ProducerConsumer3 {
+
+    public static void main(String[] args) {
+
+    }
+}
